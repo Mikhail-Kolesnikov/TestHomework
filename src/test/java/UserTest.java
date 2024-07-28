7242aefb34e8bbdd3b41958a960aa66db3783164
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-07-28 13:30:53 +0200

    Initial commit

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    private final User user = new User("Login", "Password", "mail@.com");
+
+    @Test
+    void Login_Example_Success() {
+        assertEquals("Login", user.getLogin());
+
+
+    }
+
+    @Test
+    void Password_Example_Success() {
+        assertEquals("Password", user.getPassword());
+    }
+
+    @Test
+    void Email_Example_Success() {
+        assertEquals("mail@.com", user.getEmail());
+    }
+
+    @Test
+    void Login_Example_Negative() {
+        user.setLogin("Login");
+
+        assertThrows(IllegalArgumentException.class, () -> user.setLogin("rr"));
+        System.out.println("Логин должен быть не менее 3 символов!");
+
+    }
+
+    @Test
+    void Password_Example_Negative() {
+        user.setPassword("12345678");
+
+        assertThrows(IllegalArgumentException.class, () -> user.setPassword("1234567"));
+        System.out.println("Пароль должен содержать не менее восьми символов");
+
+    }
+
+    @Test
+    void Email_Example_Negative() {
+        user.setEmail("mail@.com");
+
+        assertThrows(IllegalArgumentException.class, () -> user.setEmail("mail.com"));
+        System.out.println("Email должен содержать @");
+    }
+
+    @Test
+    void isValidPassword_try_or_false() {
+        assertTrue(user.isValidPassword("Password"));
+        assertFalse(user.isValidPassword("False"));
+    }
+}
+
+
+//Требования:
+//
+//Напишите тестовый класс UserTest, в котором будут тесты для каждого метода класса User.
+//Используйте аннотацию @Test для каждого тестового метода.
+//Используйте методы утверждений (assertEquals, assertNotEquals, assertTrue, assertFalse, assertNull, assertNotNull) для проверки правильности работы каждого метода.
+//Напишите тест для проверки исключения IllegalArgumentException в методах setLogin, setPassword и setEmail.
+//Напишите тест для проверки методаisValidPassword.
